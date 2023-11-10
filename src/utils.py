4811da981a7b989377d3249a69dae2df062c118d
James Parkington <pongs.acreage.0o@icloud.com>
2023-11-10 08:08:29 -0500

    Database creation complete

--- src/utils.py
+++ src/utils.py
@@ -576,3 +576,107 @@ def create_fct_eletric_brew(model         : str  = "./data/model/fct_electric_br
 
     except Exception as e:
         lg.error(f"Error while creating the final fact table: {e}")
+
+def create_electric_brew_db(db  : str  = "./data/sql/electric_brew.db",
+                            dfs : dict = {'meter_usage'       : meter_usage,
+                                          'locations'         : locations,
+                                          'cmp_bills'         : cmp_bills,
+                                          'dim_datetimes'     : dim_datetimes,
+                                          'dim_meters'        : dim_meters,
+                                          'dim_suppliers'     : dim_suppliers,
+                                          'fct_electric_brew' : fct_electric_brew}):
+    '''
+    This function creates a database and tables according to a predefined schema.
+    If the database already exists, it will replace the existing tables.
+
+    Parameters:
+        db_path (str) : The path where the database file should be created.
+    '''
+    
+    engine   = create_engine(f'sqlite:///{db}')
+    metadata = MetaData()
+
+    Table('meter_usage', metadata,
+          Column('service_point_id',          BigInteger),
+          Column('meter_id',                  String),
+          Column('interval_end_datetime',     DateTime),
+          Column('meter_channel',             Integer),
+          Column('kwh',                       Float),
+          Column('account_number',            String))
+
+    Table('locations', metadata,
+          Column('street',                    String),
+          Column('label',                     String),
+          Column('account_number',            String))
+
+    Table('cmp_bills', metadata,
+          Column('supplier',                  String),
+          Column('amount_due',                Float),
+          Column('service_charge',            Float),
+          Column('delivery_rate',             Float),
+          Column('supply_rate',               Float),
+          Column('interval_start',            DateTime),
+          Column('interval_end',              DateTime),
+          Column('kwh_delivered',             Float),
+          Column('total_kwh',                 BigInteger),
+          Column('pdf_file_name',             String),
+          Column('account_number',            String))
+
+    Table('dim_datetimes', metadata,
+          Column('id',                        BigInteger, primary_key = True),
+          Column('timestamp',                 DateTime),
+          Column('increment',                 Integer),
+          Column('hour',                      Integer),
+          Column('date',                      DateTime),
+          Column('week',                      Integer),
+          Column('week_in_year',              Integer),
+          Column('month',                     Integer),
+          Column('month_name',                String),
+          Column('quarter',                   Integer),
+          Column('year',                      Integer),
+          Column('period',                    String))
+
+    Table('dim_meters', metadata,
+          Column('id',                        BigInteger, primary_key = True),
+          Column('meter_id',                  String),
+          Column('service_point_id',          BigInteger),
+          Column('account_number',            String),
+          Column('street',                    String),
+          Column('label',                     String))
+
+    Table('dim_suppliers', metadata,
+          Column('id',                        BigInteger, primary_key = True),
+          Column('supplier',                  String),
+          Column('avg_supply_rate',           Float))
+
+    Table('fct_electric_brew', metadata,
+          Column('id',                        BigInteger, primary_key = True),
+          Column('dim_datetimes_id',          BigInteger, ForeignKey('dim_datetimes.id')),
+          Column('dim_meters_id',             BigInteger, ForeignKey('dim_meters.id')),
+          Column('dim_suppliers_id',          BigInteger, ForeignKey('dim_suppliers.id')),
+          Column('kwh',                       Float),
+          Column('service_charge',            Float),
+          Column('delivery_rate',             Float),
+          Column('supply_rate',               Float),
+          Column('allocated_service_charge',  Float),
+          Column('delivered_kwh_left',        Float),
+          Column('delivered_kwh_used',        Float),
+          Column('total_cost_of_delivery',    Float),
+          Column('account_number',            String))
+
+    try:
+        metadata.create_all(engine)
+
+        lg.info(f"Database and tables created at {db}")
+
+        with engine.connect() as connection:
+            for table_name, df in dfs.items():
+                df.to_sql(table_name, 
+                          con       = connection, 
+                          if_exists = 'append', 
+                          index     = 'id' in df.index.names)
+
+        # lg.info("Data inserted into tables.")
+
+    except SQLAlchemyError as e:
+        lg.error(f"Error creating database and tables: {e}")
