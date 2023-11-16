521c7e95252a541bd8100113df4e31aa966aae72
James Parkington <pongs.acreage.0o@icloud.com>
2023-11-16 17:37:36 -0500

    Explosion logic complete

--- src/testing.py
+++ src/testing.py
@@ -1,40 +1,41 @@
 from utils import *
 
-# exploded_ampion = ampion_bills.assign(date = lambda df: 
-#                         df.apply(lambda row: pd.date_range(start = row['interval_start'], 
-#                                                            end   = row['interval_end'])
-#                                                 .to_list(), axis = 1)) \
-#                                                 .explode('date')
-
-# exploded_cmp = cmp_bills.assign(date = lambda df: 
-#                         df.apply(lambda row: pd.date_range(start = row['interval_start'], 
-#                                                            end   = row['interval_end'])
-#                                                 .to_list(), axis = 1)) \
-#                                                 .explode('date')
-
-# print(ampion_bills)
-
-
 '''
-- Employ a groupby strategy for both resulting in the same dimension column names and with facts filled with 0s in place of nulls
-- Calculate `supply_rate` in `ampion_bills` while doing so
-- Join first? Then explode once?
-- Might need to confirm that interval_start and interval_end lines up with existing values from `cmp_bills` and then use those `total_kwh` values'''
+- Might need to confirm that interval_start and interval_end lines up with existing values from `cmp_bills` and then use those `total_kwh` values
+- Fill `dim_suppliers_id` with Ampion record id for all rows that receive `ampion_supply_rate`'''
 
-# Step 2: Merge and curate intermediary DataFrame
-# int_df = meter_usage.drop('account_number', axis = 1) \
-#                     .assign(timestamp = lambda df: pd.to_datetime(df['interval_end_datetime'], 
-#                                                                   format = '%m/%d/%Y %I:%M:%S %p')) \
-#                     .merge(dim_meters,      on = 'meter_id',  how = 'left').rename(columns = {'id' : 'dim_meters_id'}) \
-#                     .merge(dim_datetimes,   on = 'timestamp', how = 'left').rename(columns = {'id' : 'dim_datetimes_id'}) \
-#                     .merge(exploded_cmp,    on = ['account_number', 'date'], how = 'left') \
-#                     .merge(exploded_ampion, on = ['account_number', 'date'], how = 'left') \
-#                     .merge(dim_suppliers,   on = 'supplier',  how = 'left').rename(columns = {'id' : 'dim_suppliers_id'}) \
-#                     .sort_values(by = ['pdf_file_name', 'timestamp']) \
-#                     .fillna({'kwh_delivered'  : 0,
-#                              'service_charge' : 0,
-#                              'delivery_rate'  : 0, 
-#                              'supply_rate'    : 0})
+exploded_ampion = ampion_bills.groupby(['invoice_number', 'account_number', 'interval_start', 'interval_end'], observed = True) \
+                              .agg(ampion_kwh = ('kwh', 'sum'), ampion_price = ('price', 'sum')) \
+                              .reset_index() \
+                              .assign(ampion_supply_rate = lambda df: df['ampion_price'] / df['ampion_kwh'],
+                                      date = lambda df: df.apply(
+                                             lambda row: pd.date_range(start = row['interval_start'], 
+                                                                       end   = row['interval_end'])
+                                                           .to_list(), axis = 1)) \
+                              .explode('date')
+
+exploded_cmp = cmp_bills.assign(date = lambda df: df.apply(
+                                       lambda row: pd.date_range(start = row['interval_start'], 
+                                                                 end   = row['interval_end'])
+                                                     .to_list(), axis = 1)) \
+                        .explode('date')
+
+int_df = meter_usage.drop('account_number', axis = 1) \
+                    .assign(timestamp = lambda df: pd.to_datetime(df['interval_end_datetime'], 
+                                                                  format = '%m/%d/%Y %I:%M:%S %p')) \
+                    .merge(dim_meters,      on = 'meter_id',  how = 'left').rename(columns = {'id' : 'dim_meters_id'}) \
+                    .merge(dim_datetimes,   on = 'timestamp', how = 'left').rename(columns = {'id' : 'dim_datetimes_id'}) \
+                    .merge(exploded_cmp,    on = ['account_number', 'date'], how = 'left') \
+                    .merge(dim_suppliers,   on = 'supplier',  how = 'left').rename(columns = {'id' : 'dim_suppliers_id'}) \
+                    .sort_values(by = ['pdf_file_name', 'timestamp']) \
+                    .fillna({'kwh_delivered'      : 0,
+                             'service_charge'     : 0,
+                             'delivery_rate'      : 0, 
+                             'supply_rate'        : 0,
+                             'ampion_kwh'         : 0,
+                             'ampion_supply_rate' : 0})
+
+print(int_df)
 
 # # Steps 3 & 4: Calculate metrics and total cost of delivery
 # fct_df = (
