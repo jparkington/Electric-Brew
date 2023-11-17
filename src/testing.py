070436a29fbb6752f6e65b2a92ee0d0c01f69815
James Parkington <pongs.acreage.0o@icloud.com>
2023-11-17 13:54:39 -0500

    Refactor of `int_df` complete

--- src/testing.py
+++ src/testing.py
@@ -6,61 +6,44 @@ from utils import *
 - Ids from both exploded cmp and ampion will need to be coalesced and then renamed
 '''
 
-# exploded = {s: df for s, df in dim_bills.explode('billing_interval').groupby('source')}
+explode = {s: df for s, df in dim_bills.explode('billing_interval')
+                                       .assign(date = lambda x: pd.to_datetime(x['billing_interval']))
+                                       .groupby('source')}
+
+bill_fields = ['account_number', 'date']
+int_df = meter_usage.assign(timestamp = lambda df: pd.to_datetime(df['interval_end_datetime'], format = '%m/%d/%Y %I:%M:%S %p')) \
+                    .merge(dim_datetimes,     on = 'timestamp', how = 'left', suffixes = ('', '_dat')) \
+                    .merge(dim_meters,        on = 'meter_id',  how = 'left', suffixes = ('', '_met')) \
+                    .merge(explode['CMP'],    on = bill_fields, how = 'left', suffixes = ('', '_cmp')) \
+                    .merge(explode['Ampion'], on = bill_fields, how = 'left', suffixes = ('', '_amp')) \
+                    .apply(lambda col: col.fillna(0) if col.dtype.kind in 'biufc' else col)
+
+print(int_df.columns)
 
 
-# int_df = meter_usage.drop('account_number', axis = 1) \
-#                     .assign(timestamp = lambda df: pd.to_datetime(df['interval_end_datetime'], 
-#                                                                   format = '%m/%d/%Y %I:%M:%S %p')) \
-#                     .merge(dim_meters,      on = 'meter_id',  how = 'left').rename(columns = {'id' : 'dim_meters_id'}) \
-#                     .merge(dim_datetimes,   on = 'timestamp', how = 'left').rename(columns = {'id' : 'dim_datetimes_id'}) \
-#                 #     .merge(exploded_bills,    on = ['account_number', 'date'], how = 'left') \
-#                 #     .merge(exploded_ampion, on = ['account_number', 'date'], how = 'left') \
+# # Add columns to fct_df using calculations from int_df
+# int_df['total_recorded_kwh'] = int_df.groupby(['pdf_file_name', 'kwh_delivered'])['kwh'].transform('sum')
 
-# # print(exploded['CMP'])
+# int_df['allocated_service_charge'] = int_df['service_charge'] * int_df['kwh'] / int_df['total_recorded_kwh']
 
-# common_dims = ['invoice_number', 'account_number', 'interval_start', 'interval_end', 'supplier']
+# int_df['delivered_kwh_left'] = int_df.groupby(['pdf_file_name', 'kwh_delivered'], group_keys=False) \
+#                                      .apply(lambda g: (g.sort_values(by=['pdf_file_name', 'timestamp'], ascending=True)['kwh_delivered'].iloc[0] - 
+#                                                        g.sort_values(by=['pdf_file_name', 'timestamp'], ascending=True)['kwh'].cumsum())
+#                                                        .clip(lower=0))
 
-# # Standardize `cmp_bills`
-# df1 = cmp_bills.groupby(common_dims, observed=True) \
-#                 .agg(kwh_delivered  = ('kwh_delivered',  'sum'),
-#                         service_charge = ('service_charge', 'sum'), 
-#                         delivery_rate  = ('delivery_rate',  'mean'),
-#                         supply_rate    = ('supply_rate',    'mean')) \
-#                 .reset_index()
+# int_df['delivered_kwh_used'] = np.minimum(int_df['kwh'], int_df['delivered_kwh_left']).clip(lower=0)
 
-# print(df1, '\n', cmp_bills)
 
-# Do the .fillna differently
-                #     .fillna({'kwh_delivered'      : 0,
-                #              'service_charge'     : 0,
-                #              'delivery_rate'      : 0, 
-                #              'supply_rate'        : 0,
-                #              'ampion_kwh'         : 0,
-                #              'ampion_supply_rate' : 0})
+fct_df = pd.DataFrame(index = int_df.index)
 
-# Decompose the .assign and use key assignment
-# fct_df = (
-#     int_df.sort_values(by = ['pdf_file_name', 'timestamp']) \
-#           .assign(
-#         total_recorded_kwh       = int_df.groupby(['pdf_file_name', 'kwh_delivered'])['kwh'].transform('sum'),
-#         allocated_service_charge = lambda x: x['service_charge'] * x['kwh'] / x['total_recorded_kwh'],
-#         delivered_kwh_left       = int_df.groupby(['pdf_file_name', 'kwh_delivered'], group_keys = False)
-#                                             .apply(lambda g: (g['kwh_delivered'].iloc[0] - g['kwh'].iloc[::-1].cumsum())
-#                                             .clip(lower = 0)),
-#         delivered_kwh_used       = lambda x: np.minimum(x['kwh'], x['delivered_kwh_left']).clip(lower = 0),
-#         total_cost_of_delivery   = lambda x: x['delivered_kwh_used'] * (x['delivery_rate'] + x['supply_rate']) +
-#                                                 x['allocated_service_charge']))
-        # .sort_values(by = ['dim_meters_id', 'dim_datetimes_id']) \
-        # [[
-        #         'dim_datetimes_id',
-        #         'dim_meters_id',
-        #         'dim_suppliers_id',
-        #         'account_number',
-        #         'kwh',
-        #         'total_cost_of_delivery'
-        # ]]
+# Add other columns from int_df as needed
+fct_df['dim_datetimes_id'] = int_df['id']
+fct_df['dim_meters_id']    = int_df['id_met']
+fct_df['dim_bills_id']     = int_df['id_cmp']
+fct_df['account_number']   = int_df['account_number']
+fct_df['kwh']              = int_df['kwh']
 
+# int_df['total_cost_of_delivery'] = int_df['delivered_kwh_used'] * (int_df['delivery_rate'] + int_df['supply_rate']) + int_df['allocated_service_charge']
 
 # # Step 5: Create a unique identifier `id` for each row
 # fct_df.insert(0, 'id', range(1, len(fct_df) + 1))
